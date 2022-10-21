cf674e7945d076b154ebdc661d6e9318bdd6ff9d
Leva <parimalleva@gmail.com>
2022-10-21 13:56:31 +0300

    2 of 8 stages completed

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,2 +1,7 @@
-print("H A N G M A N")
-print("The game will be available soon.")
+print('H A N G M A N')
+print('Guess the word: ')
+word = input()
+if word == "python":
+    print('You survived!')
+else:
+    print('You lost!')
