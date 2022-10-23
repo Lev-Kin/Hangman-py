9902df300fb0404c20a0ea6b3dbbe3c843aabfba
Leva <parimalleva@gmail.com>
2022-10-23 12:40:20 +0300

    3 of 8 stages completed

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,7 +1,13 @@
+import random
+
 print('H A N G M A N')
+
+words = ['python', 'java', 'swift', 'javascript']
+x = random.choice(words)
+
 print('Guess the word: ')
 word = input()
-if word == "python":
+if word == x:
     print('You survived!')
 else:
     print('You lost!')
---
+++ Topics/Declaring a function/Fahrenheit/main.py
@@ -0,0 +1,4 @@
+def fahrenheit_to_celsius(fahrenheit):
+    fahrenheit = round((fahrenheit - 32) * (5 / 9), 3)
+    return fahrenheit
+
