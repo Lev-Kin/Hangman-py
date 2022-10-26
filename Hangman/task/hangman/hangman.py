69fb80e8f7c294861165ddad593d337153bd39ea
Leva <parimalleva@gmail.com>
2022-10-26 11:59:20 +0300

    5 of 8 stages completed

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,21 +1,91 @@
 import random
 
-print('H A N G M A N')
 
-words = ['python', 'java', 'swift', 'javascript']
-x = random.choice(words)
-y = x.format()
-if x == 'python':
-    print('Guess the word pyt---:')
-elif x == 'java':
-    print('Guess the word jav-:')
-elif x == 'swift':
-    print('Guess the word swi--:')
-if x == 'javascript':
-    print('Guess the word jav-------:')
+def loadWords():
+    word_list = ['python', 'java', 'swift', 'javascript']
+    return random.choice(word_list)
 
-word = input()
-if word == x:
-    print('You survived!')
-else:
-    print('You lost!')
+
+word = loadWords()
+
+
+def isWordGuessed(secretWord, lettersGuessed):
+    c = 0
+    for i in lettersGuessed:
+        if i in secretWord:
+            c += 1
+    if c == len(secretWord):
+        return True
+    else:
+        return False
+
+
+def getGuessedWord(secretWord, lettersGuessed):
+    s = []
+    for i in secretWord:
+        if i in lettersGuessed:
+            s.append(i)
+    ans = ''
+    for i in secretWord:
+        if i in s:
+            ans += i
+        else:
+            ans += '-'
+    return ans
+
+
+def getAvailableLetters(lettersGuessed):
+    import string
+    ans = list(string.ascii_lowercase)
+    for i in lettersGuessed:
+        ans.remove(i)
+    return ''.join(ans)
+
+
+def hangman(secretWord):
+    print("H A N G M A N")
+    print()
+    hidden = len(word)
+    hint = "-" * hidden
+    print(hint)
+
+    global lettersGuessed
+    turns = 0
+    lettersGuessed = []
+
+    while 8 - turns > 0:
+        turns += 1
+        if isWordGuessed(secretWord, lettersGuessed):
+            continue
+
+        else:
+
+            guess = str(input("Input a letter: ")).lower()
+            print()
+
+            if guess in secretWord and guess not in lettersGuessed:
+
+                lettersGuessed.append(guess)
+                print(getGuessedWord(secretWord, lettersGuessed))
+
+            else:
+                if (guess in secretWord and guess in lettersGuessed):
+                    print(getGuessedWord(secretWord, lettersGuessed))
+                    continue
+
+                lettersGuessed.append(guess)
+                print("That letter doesn't appear in the word.")
+                print()
+                if 8 - turns > 0:
+                    print(getGuessedWord(secretWord, lettersGuessed))
+
+        if 8 - turns == 0:
+            print("Thanks for playing!")
+            break
+
+        else:
+            continue
+
+
+secretWord = word.lower()
+hangman(secretWord)
