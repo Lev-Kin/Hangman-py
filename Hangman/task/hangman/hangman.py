b935d52a21e2cc94724d0dd7773e6daee1441d12
Leva <parimalleva@gmail.com>
2022-10-29 10:56:38 +0300

    6 of 8 stages completed

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,91 +1,38 @@
 import random
 
+words = ['python', 'java', 'swift', 'javascript']
+hidden = random.choice(words)
+correct_word = ''
+all_letters = ''
+tries = 0
 
-def loadWords():
-    word_list = ['python', 'java', 'swift', 'javascript']
-    return random.choice(word_list)
-
-
-word = loadWords()
-
-
-def isWordGuessed(secretWord, lettersGuessed):
-    c = 0
-    for i in lettersGuessed:
-        if i in secretWord:
-            c += 1
-    if c == len(secretWord):
-        return True
-    else:
-        return False
-
-
-def getGuessedWord(secretWord, lettersGuessed):
-    s = []
-    for i in secretWord:
-        if i in lettersGuessed:
-            s.append(i)
-    ans = ''
-    for i in secretWord:
-        if i in s:
-            ans += i
+print("H A N G M A N")
+while tries < 8:
+    result = ''
+    for l in hidden:
+        if l in correct_word:
+            result += l
         else:
-            ans += '-'
-    return ans
+            result += '-'
 
-
-def getAvailableLetters(lettersGuessed):
-    import string
-    ans = list(string.ascii_lowercase)
-    for i in lettersGuessed:
-        ans.remove(i)
-    return ''.join(ans)
-
-
-def hangman(secretWord):
-    print("H A N G M A N")
     print()
-    hidden = len(word)
-    hint = "-" * hidden
-    print(hint)
+    print('{}'.format(result))
+    if set(hidden) == set(correct_word):
+        print('You guessed the word!\nYou survived!')
+        break
+    while True:
+        user_letter = input('Input a letter: ')
+        all_letters += user_letter
+        break
+    if user_letter not in hidden:
+        tries += 1
+        print("That letter doesn't appear in the word")
+    elif user_letter in correct_word:
+        tries += 1
+        print('No improvements')
+    else:
+        correct_word += user_letter
 
-    global lettersGuessed
-    turns = 0
-    lettersGuessed = []
-
-    while 8 - turns > 0:
-        turns += 1
-        if isWordGuessed(secretWord, lettersGuessed):
-            continue
-
-        else:
-
-            guess = str(input("Input a letter: ")).lower()
-            print()
-
-            if guess in secretWord and guess not in lettersGuessed:
-
-                lettersGuessed.append(guess)
-                print(getGuessedWord(secretWord, lettersGuessed))
-
-            else:
-                if (guess in secretWord and guess in lettersGuessed):
-                    print(getGuessedWord(secretWord, lettersGuessed))
-                    continue
-
-                lettersGuessed.append(guess)
-                print("That letter doesn't appear in the word.")
-                print()
-                if 8 - turns > 0:
-                    print(getGuessedWord(secretWord, lettersGuessed))
-
-        if 8 - turns == 0:
-            print("Thanks for playing!")
-            break
-
-        else:
-            continue
-
-
-secretWord = word.lower()
-hangman(secretWord)
+if tries == 8:
+    print('You lost!')
+    exit()
---
+++ Topics/Multi-line programs/A week/main.py
@@ -0,0 +1,4 @@
+day_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+for day in day_week:
+    print(day)
+    print()
